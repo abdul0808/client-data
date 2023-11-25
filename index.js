0ca4ba82277e4d12a5808b8cfd9a4fcd62ead8a4
Abdul <abdur9718rahman@gmail.com>
2023-11-25 16:09:35 +0530

    index updated

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
-const jsonServer = require("jsin-server");             //importing json-server library
+const jsonServer = require("json-server");             //importing json-server library
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middleware = jsonServer.defaults();
 const port = process.env.PORT || 8080;                //choose port from here like 8080, 3031
-
 
 server.use(middleware);
 server.use(router);
